d98f570b0800046097a6d588dacaa5e9cd7a4adb
sibgatulin-danila <sibgat.d.s@gmail.com>
2019-06-07 21:14:12 +0700

    слайдер заработал

---
+++ script.js
@@ -0,0 +1,11 @@
+var mySwiper = new Swiper('.swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+     el: '.swiper-scrollbar',
+   },
+});
