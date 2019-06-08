2308fd1ad3a66552812b4705077e5f1b00d76be9
sibgatulin-danila <sibgat.d.s@gmail.com>
2019-06-08 09:13:20 +0700

    Выровненный слайдер

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var mySwiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
   loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
