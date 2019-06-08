ce81f594002b6c5fb4dad519022a002db12f7da3
sibgatulin-danila <sibgat.d.s@gmail.com>
2019-06-08 17:07:34 +0700

    добавлены изображения для слайдера выделена область под слайдер

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ var mySwiper = new Swiper('#first-window .swiper-container', {
      el: '.swiper-scrollbar',
    },
 });
+
+var galleryThumbs = new Swiper('#second-window .gallery-thumbs', {
+  spaceBetween:17,
+  slidesPerView: 4,
+  loop: false,
+  freeMode: true,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+});
+
+var galleryTop = new Swiper('#second-window .gallery-top', {
+      spaceBetween: 10,
+      loop:false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+    });
