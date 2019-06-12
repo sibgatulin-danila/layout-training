a0352ec55ab74915d0297b5e547f82a39a8fd2ce
sibgatulin-danila <sibgat.d.s@gmail.com>
2019-06-12 15:28:33 +0700

    Исправленный код html и script обновлены файлы шрифтов

--- script.js
+++ script.js
@@ -1,52 +1,53 @@
 var mySwiper = new Swiper('#first-window .swiper-container', {
-  direction: 'horizontal',
-  loop: false,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  scrollbar: {
-     el: '.swiper-scrollbar',
-   },
+    direction: 'horizontal',
+    loop: false,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
 });
 
 var galleryThumbs = new Swiper('#second-window .gallery-thumbs', {
-  spaceBetween:10,
-  slidesPerView: 4,
-  loop: false,
-  freeMode: true,
-  watchSlidesVisibility: true,
-  watchSlidesProgress: true,
+    spaceBetween: 10,
+    slidesPerView: 4,
+    loop: false,
+    freeMode: true,
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true,
 });
 
 var galleryTop = new Swiper('#second-window .gallery-top', {
-      spaceBetween: 10,
-      loop:false,
-      navigation: {
+    spaceBetween: 10,
+    loop: false,
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-      thumbs: {
+    },
+    thumbs: {
         swiper: galleryThumbs,
-      },
+    },
 });
 
 $(document).ready(function() {
+
     $('#dd-size').click(function() {
         if (this.className == 'disabled') {
             let len = $(this).find('li').length;
             $(this).height(len * 37 + 35);
             this.className = 'enabled';
-            $('#dd-size .arrow-down:first').prop('className', 'arrow-up');
-            $('#dd-size .dropdown:first').prop('className', 'dropdown active');
+            $('#dd-size .arrow-down:first').prop('className', 'arrow-up arrow');
+            $('#dd-size .dropdown:first').prop('className', 'dropdown drop');
         } else {
             $(this).height(29);
             this.className = 'disabled';
-            $('#dd-size .arrow-up:first').prop('className', 'arrow-down');
-            $('#dd-size .dropdown:first').prop('className', 'dropdown non-active');
+            $('#dd-size .arrow-up:first').prop('className', 'arrow-down arrow');
+            $('#dd-size .dropdown:first').prop('className', 'dropdown drop-off');
         }
     })
 });
